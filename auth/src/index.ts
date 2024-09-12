b9baf9fd2da3bd4c7e6286fb37df252bce152724
DebjitRoy <debjitroy.dev@gmail.com>
2024-09-11 19:44:22 -0500

    async error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import "express-async-errors"; // allows to throw inside async
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,8 +18,25 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
+app.all("*", async () => {
+  // we're able to throw inside async due to express-async-errors
+  // without that, we'd have to next(new NotFoundError()) [from (req,res,next)]
+  throw new NotFoundError();
+});
+
 app.use(errorHandler); // generic error handler mw
 
-app.listen(3000, () => {
-  console.log("Listening on 3000!!");
-});
+const start = async () => {
+  try {
+    // mongo db cluster ip service name
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on 3000!!");
+  });
+};
+
+start();
